d852f2bc5069fb510033b697f067e12c04fe1b49
goenning <me@goenning.net>
2024-02-04 19:16:26 +0000

    Fixed an issue where `with_panic_hook` would swallow the panic after logging it

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.5.1
+
+* Fixed an issue where `with_panic_hook` would swallow the panic after logging it
+
 ## 0.5.0
 
 * Fixed an issue where `with_panic_hook` would panic
--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,7 @@ impl Builder {
           client.start_polling(cfg.flush_interval);
           
           if let Some(hook) = self.panic_hook {
+            let default_panic = std::panic::take_hook();
             let hook_client = client.clone();
             std::panic::set_hook(Box::new(move |info| {
                 let msg = get_panic_message(info);
@@ -85,6 +86,8 @@ impl Builder {
 
                 let _ = hook_client.flush();
                 sleep(std::time::Duration::from_secs(2));
+
+                default_panic(info);
             }));
           }
 
